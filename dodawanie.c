41b9cec3af92df08fe6e03d204158bada9d86846
KrissM1987 <krissm@poczta.fm>
2018-10-11 17:10:07 +0200

    dodawanie prgram

---
+++ dodawanie.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+const int y=5;
+const int z=-9;
+printf(" wynik wynosi : %i\n ", y+z);
+int a,b,c;
+
+printf ("wpisz liczbę : ");
+scanf ("%d", &a);
+printf("wpisz 2 liczbę :");
+scanf ("%d", &b);
+
+
+printf (" wynik wynosi : %d\n", c=a+b);
+
+
+return 0;
+}
